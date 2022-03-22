3a9d85644e2ec2f6d7692e150afd3e2ab8e6bedc
Samuli Pietarinen <samuli.pietarinen@iki.fi>
2022-03-22 10:01:01 +0200

    Initial commit, added files

---
+++ my-unzip.c
@@ -0,0 +1,12 @@
+/*
+ * my-unzip.c
+ * CT30A3370
+ * 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    
+}
---
+++ my-zip.c
@@ -0,0 +1,12 @@
+/*
+ * my-zip.c
+ * CT30A3370
+ * 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    
+}
