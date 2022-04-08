28c849aac3810a08d80f554ae0d46e740f4c2165
Samuli Pietarinen <samuli.pietarinen@iki.fi>
2022-04-08 14:44:55 +0300

    Implemented my-zip and my-unzip. Added test textfile for them.

--- my-unzip.c
+++ my-unzip.c
@@ -6,7 +6,73 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+int unzip(FILE *fp) {
+    // variable for char
+    char c;
+    // number of chars
+    uint32_t count;
+    // return value for fread
+    size_t ret;
+    while(1) {
+        // Reading number
+        if ((ret = fread(&count, sizeof(uint32_t), sizeof(uint32_t), fp)) == 0) {
+            break;
+        }
+        // Check for fread success
+        // Source fread man page
+        if (ret != sizeof(uint32_t)) {
+            fprintf(stderr, "fread() failed: %zu\n", ret);
+            exit(1);
+        }
+
+        // Reading char
+        if ((ret = fread(&c, sizeof(char), sizeof(char), fp)) == 0) {
+            break;
+        }
+        // Check for fread success
+        // Source fread man page
+        if (ret != sizeof(char)) {
+            fprintf(stderr, "fread() failed: %zu\n", ret);
+            exit(1);
+        }
+
+        for (int i = 0; i < count; i++) {
+            fprintf(stdout, "%c", c);
+        }
+    }
+    // Print newline at the end
+    fprintf(stdout, "\n");
+    return 0;
+}
 
 int main(int argc, char **argv) {
-    
+    // Define variables
+    FILE *fp;
+
+    // Check amount of arguments
+    if (argc < 2) {
+        return 0;
+    }
+
+    // Loop through all given files
+    for (int i = 1; i < argc; i++) {
+        // Open file, handle possible errors
+        if ((fp = fopen(argv[i], "r")) == NULL) {
+            perror("my-unzip: cannot open file\n");
+            return 1;
+        }
+        if (unzip(fp) != 0) {
+            perror("my-unzip: weird error\n");
+            return 1;
+        }
+        if (fclose(fp) != 0) {
+            perror("my-unzip: unable to close file\n");
+            return 1;
+        }
+    }
+
+    // End program
+    return 0;
 }
--- my-zip.c
+++ my-zip.c
@@ -6,7 +6,66 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+int zip(FILE *fp) {
+    // current and previous char
+    char c, co;
+    uint32_t i = 0;
+    while ((c = fgetc(fp))) {
+        // Begin of the file
+        if (i == 0) {
+            co = c;
+            i = 1;
+            continue;
+        }
+        // If char equals previous
+        if (c == co) {
+            i++;
+            continue;
+        }
+        // If char differs from previous
+        if (c != co) {
+            fwrite(&i, sizeof(int32_t), sizeof(int32_t), stdout);
+            fwrite(&co, sizeof(char), sizeof(char), stdout);
+            co = c;
+            i = 1;
+            // If EOF
+            if (c == EOF) {
+                break;
+            }
+            continue;
+        }
+    }
+    return 0;
+}
 
 int main(int argc, char **argv) {
-    
+    // Define variables
+    FILE *fp;
+
+    // Check amount of arguments
+    if (argc < 2) {
+        return 0;
+    }
+
+    // Loop through all given files
+    for (int i = 1; i < argc; i++) {
+        // Open file, handle possible errors
+        if ((fp = fopen(argv[i], "r")) == NULL) {
+            perror("my-zip: cannot open file\n");
+            return 1;
+        }
+        if (zip(fp) != 0) {
+            perror("my-zip: weird error\n");
+            return 1;
+        }
+        if (fclose(fp) != 0) {
+            perror("my-zip: unable to close file\n");
+            return 1;
+        }
+    }
+
+    // End program
+    return 0;
 }
